ceffa50533dc3f23b50b4ede38131f3139a3a9aa
hirexpie <bdrik61@gmail.com>
2025-12-07 14:10:20 +0500

    aaa

--- src/db/entity/user.ts
+++ src/db/entity/user.ts
@@ -9,7 +9,12 @@ interface IUser extends Document {
 const userSchema = new Schema<IUser>({
     name: { type: String, required: true },
     TeleId: { type: String, required: true, unique: true },
-    balance: { type: Number, default: 3000 },
+    balance: {
+        type: Number,
+        default: 3000,
+        set: (v: number) => Number(v.toFixed(2))
+    },
+
 });
 
 export const User = model<IUser>("User", userSchema);
